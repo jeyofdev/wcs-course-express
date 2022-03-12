da0d9bb67609bff25932c377dd941c18c2f69397
jeyofdev <jgregoire.pro@gmail.com>
2022-03-12 11:22:49 +0100

    feat : filter wilders by city

--- client/src/components/ui/Alert.jsx
+++ client/src/components/ui/Alert.jsx
@@ -1,6 +1,6 @@
 const Alert = ({ children }) => {
     return (
-        <div className="relative p-4 mb-4 rounded-md border border-blue-300 bg-blue-100 w-full text-blue-900">
+        <div className="relative mx-4 p-4 mb-4 rounded-md border border-blue-300 bg-blue-100 text-blue-900">
             {children}
         </div>
     );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,22 +4,32 @@ import Alert from '../components/ui/Alert';
 import { getAllWilders } from '../utils/queries';
 import WilderList from '../components/wilders/WilderList';
 import { PlusCircleIcon } from '@heroicons/react/solid';
+import { optionsCities } from '../datas';
 
 const Home = () => {
     const [wilders, setWilders] = useState([]);
     const [loading, setLoading] = useState(true);
     const [search, setSearch] = useState('');
+    const [filter, setFilter] = useState('all');
 
     const fetchDatas = async () => {
         try {
             const datas = await getAllWilders();
             setWilders(
-                datas.filter((wilder) => {
-                    return wilder.name
-                        .slice(0, search.length)
-                        .toLowerCase()
-                        .includes(search.toLowerCase());
-                })
+                datas
+                    .filter((wilder) =>
+                        wilder.name
+                            .slice(0, search.length)
+                            .toLowerCase()
+                            .includes(search.toLowerCase())
+                    )
+                    .filter((wilderFiltered) =>
+                        filter === 'all'
+                            ? wilderFiltered
+                            : wilderFiltered.city
+                                  .toLowerCase()
+                                  .includes(filter.toLowerCase())
+                    )
             );
         } catch (error) {
         } finally {
@@ -31,19 +41,40 @@ const Home = () => {
         setSearch(e.target.value);
     };
 
+    const handleChangeSelect = (e) => {
+        setFilter(e.target.value);
+    };
+
     useEffect(() => {
         fetchDatas();
-    }, [search]);
+    }, [search, filter]);
 
     return (
         <>
-            <input
-                type="text"
-                className="bg-white border border-gray-300 ml-4 w-1/3 p-4 rounded-lg"
-                placeholder="your search"
-                name="search"
-                onChange={handleSearch}
-            />
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mb-8">
+                <input
+                    type="text"
+                    className="bg-white border border-gray-300 p-4 rounded-lg sm:mx-4 px-4 my-4 max-w-300px mx-auto sm:max-w-auto"
+                    placeholder="your search"
+                    name="search"
+                    onChange={handleSearch}
+                />
+
+                <select
+                    className="bg-white border border-gray-300 p-4 rounded-lg sm:mx-4 px-4 my-4 max-w-300px mx-auto sm:max-w-auto"
+                    name="city"
+                    value={filter}
+                    onChange={handleChangeSelect}
+                >
+                    <option value="all">All city</option>
+                    {optionsCities.map((city) => (
+                        <option key={city.id} value={city.value}>
+                            {city.value.slice(0, 1).toUpperCase() +
+                                city.value.slice(1)}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
             {!loading ? (
                 <WilderList wilders={wilders} refetch={fetchDatas} />
