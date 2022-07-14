e3b8bf8087e8fb78437d0646f216843c5033c264
jeyofdev <jgregoire.pro@gmail.com>
2022-07-14 12:34:00 +0200

    config : add service database-test in config docker

--- api/src/config/getAppolloServer.ts
+++ api/src/config/getAppolloServer.ts
@@ -1,14 +1,15 @@
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { Container } from 'typedi';
+import WilderResolver from '../resolvers/wilder.resolver';
 
 const getApolloServer = async () => {
   const schema = await buildSchema({
-    resolvers: [`${process.cwd()}/src/**/*.resolver.{ts,js}`],
+    resolvers: [WilderResolver],
     container: Container,
   });
 
-  const apolloServer = await new ApolloServer({ schema });
+  const apolloServer = new ApolloServer({ schema });
   return apolloServer;
 };
 
